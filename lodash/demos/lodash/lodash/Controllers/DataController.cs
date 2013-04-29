de7b3dd5e24982e2135bc26917f93a9403ce6428
Brian Woodward <brian.woodward@gmail.com>
2013-04-29 15:41:10 -0400

    starting the data api and making sure I can call my helper from the console

---
+++ lodash/demos/lodash/lodash/Controllers/DataController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace lodash.Controllers
+{
+    public class DataController : ApiController
+    {
+        public List<object> Get(string id)
+        {
+            var results = new List<object>();
+            results.Add(id);
+            return results;
+        }
+    }
+}
---
+++ lodash/demos/lodash/lodash/Scripts/helpers.js
@@ -0,0 +1,13 @@
+﻿(function ($, _) {
+    var helpers = window.helpers = window.helpers || {};
+
+    helpers.getData = function (id, callback) {
+        $.get('api/data/' + id).success(function (data) {
+            callback(data);
+        });
+    };
+
+    $(function () {
+        console.log('helpers loaded');
+    });
+})(window.$, window._);
